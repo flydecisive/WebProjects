fdfa409d1655deba66cedae0d72757329b3ef5d4
flydecisive <flydecisive@yandex.ru>
2022-10-10 20:57:30 +0300

    Задачи по js из 3-го урока

---
+++ JS/skypro/hw3/task1.js
@@ -0,0 +1,31 @@
+// Задача 1
+const arr = [];
+let number;
+
+arr[0] = number;
+
+// Задача 2
+for (let i = 0; i < 2; i++) {
+    console.log('Привет');
+}
+
+// Задача 3
+let i = 1;
+
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+// Задача 4
+let j = 7;
+
+do {
+    console.log(j);
+    j++;
+} while (j < 23)
+
+// Задача 5
+const randomNumber = () => Math.floor(Math.random() * 10 + 1);
+
+console.log(randomNumber());
