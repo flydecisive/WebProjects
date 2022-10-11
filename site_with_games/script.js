674fe0eb7a7415f1772d2b93e38c52f6a381f025
flydecisive <flydecisive@yandex.ru>
2022-10-11 20:39:42 +0300

    Добавил цикл в игру

--- site_with_games/script.js
+++ site_with_games/script.js
@@ -1,19 +1,26 @@
 const seasons = () => {
-    let season = Number(prompt('Введите номер месяца'));
-    if (season < 1 || season > 12) {
-        alert('Нужно вводить номера от 1 до 12');
-    } else {
-        season = Number(season);
-        if (season === 1 || season === 12 || season === 2) {
-            alert('Зима');
-        } else if (season > 2 & season <= 5) {
-            alert('Весна');
-        } else if (season > 5 & season <= 8) {
-            alert('Лето');
-        } else if (season > 8 & season <= 11) {
-            alert('Осень');
+    let answer = false;
+    while (answer !== true) {
+        let season = Number(prompt('Введите номер месяца'));
+        if (season < 1 || season > 12) {
+            alert('Нужно вводить номера от 1 до 12');
         } else {
-            alert('Нужно вводить только номер месяца от 1 до 12');
+            season = Number(season);
+            if (season === 1 || season === 12 || season === 2) {
+                alert('Зима');
+                answer = true;
+            } else if (season > 2 & season <= 5) {
+                alert('Весна');
+                answer = true;
+            } else if (season > 5 & season <= 8) {
+                alert('Лето');
+                answer = true;
+            } else if (season > 8 & season <= 11) {
+                alert('Осень');
+                answer = true;
+            } else {
+                alert('Нужно вводить только номер месяца от 1 до 12');
+            }
         }
     }
 }
