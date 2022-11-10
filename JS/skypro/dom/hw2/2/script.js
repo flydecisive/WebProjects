0c32cbc2ef804474ef306677d43c90356919dd9d
flydecisive <flydecisive@yandex.ru>
2022-11-10 20:25:20 +0300

    2 задача

---
+++ JS/skypro/dom/hw2/2/script.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('btn');
+const img = document.getElementById('img');
+
+button.onclick = () => {
+    button.classList.add('hide');
+    img.classList.remove('hide');
+    img.src = 'https://fydi.ru/wp-content/uploads/2021/08/koty-i-koshki-79.jpg';
+}
