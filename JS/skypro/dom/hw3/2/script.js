98d206f6e9864934cefb8b97021c2a7b079279a0
flydecisive <flydecisive@yandex.ru>
2022-11-16 18:02:17 +0300

    решил задачи из курса

---
+++ JS/skypro/dom/hw3/2/script.js
@@ -0,0 +1,35 @@
+const button = document.getElementsByClassName('button')[0];
+
+function resetClicks() {
+    button.dataset.clicks = 0;
+}
+
+function deleteItem() {
+    clicks.shift();
+}
+
+function buttonClickHandler() {
+    let clicks = +button.dataset.clicks;
+    clicks += 1;
+    button.dataset.clicks = clicks;
+}
+
+const clicks = [];
+
+button.onclick = function () {
+    let date = new Date();
+    let seconds = date.getSeconds();
+    seconds = parseInt(seconds);
+    buttonClickHandler();
+    clicks.push(seconds);
+    if (clicks.length > 2) {
+        deleteItem();
+        let deltaTime = parseInt(clicks[0]) - parseInt(clicks[1]);
+
+        if (deltaTime === 0) {
+            resetClicks();
+        }
+    }
+}
+
+
