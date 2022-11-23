1749f6e621cacacb3075ae4b7eee5bf45ff5226f
flydecisive <flydecisive@yandex.ru>
2022-11-23 11:56:56 +0300

    .

--- JS/skypro/dom/hw3/1/script.js
+++ JS/skypro/dom/hw3/1/script.js
@@ -1,4 +1,5 @@
 const bingo = document.getElementsByClassName('bingo')[0];
+const text = document.getElementsByClassName('hide')[0];
 
 function changeBg() {
     const symbols = '0123456789ABCDEF';
@@ -6,7 +7,7 @@ function changeBg() {
     for (let i = 0; i < 6; i++) {
         color += symbols[Math.floor(Math.random() * 16)];
     }
-    console.log(color);
+    text.textContent = color;
     return color;
 }
 
@@ -14,14 +15,10 @@ function setBg() {
     bingo.style.background = changeBg();
 }
 
-
-
 let timer = setInterval(() => setBg(), 100);
 
 bingo.onclick = function () {
     clearInterval(timer);
-    const text = document.getElementsByClassName('hide')[0];
     text.classList.remove('hide');
     text.classList.add('text');
-    text.textContent = changeBg();
 }
