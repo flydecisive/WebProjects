ebc28926f1a98829bbb254d8f2996ac8f938e4c4
flydecisive <flydecisive@yandex.ru>
2023-01-18 21:31:51 +0300

    Виджет с погодой

--- JS/skypro/dom/hw14/weather_widget/script.js
+++ JS/skypro/dom/hw14/weather_widget/script.js
@@ -8,27 +8,38 @@ const weatherStatuses = {
     clouds: 'img/Cloudy.png',
 };
 
+
+
 document.addEventListener('DOMContentLoaded', function () {
     const widget = document.querySelector('.widget');
-    const widgetCity = widget.querySelector('.widget__city');
+    const suggest = widget.querySelector('.suggest');
+    const button = widget.querySelector('.widget__button');
     const widgetWeatherStatus = widget.querySelector('.widget__weather-status');
     const widgetTemperature = widget.querySelector('.widget__temperature');
     const widgetWeatherMain = widget.querySelector('.widget__weather-main');
     const widgetWind = widget.querySelector('.widget__wind');
-    navigator.geolocation.getCurrentPosition(
-        function (data) {
-            request({
-                url: 'data.json',
-                onSuccess: (data) => {
-                    const weatherStatus = data.weather[0].main;
-                    widgetCity.textContent = `${data.name}, ${data.sys.country}`;
-                    widgetTemperature.textContent = `${Math.floor(data.main.temp - 273.15)} ºC`;
-                    widgetWeatherMain.textContent = weatherStatus[0].toUpperCase() + weatherStatus.slice(1);
-                    widgetWeatherStatus.src = weatherStatuses[weatherStatus.toLowerCase()];
-                    widgetWind.textContent = `Ветер: ${data.wind.speed} м/c`;
-                }
-            });
-        });
+
+    button.addEventListener('click', function () {
+        request({
+            url: `http://api.openweathermap.org/geo/1.0/direct?q=${suggest.value},RU&appid=7a4da1fe147dd807bc54e1122dff4722`,
+            onSuccess: (data) => {
+                console.log(data[0].lat);
+                console.log(data[0].lon);
+                request({
+                    url: `https://api.openweathermap.org/data/2.5/weather?lat=${data[0].lat}&lon=${data[0].lon}&appid=7a4da1fe147dd807bc54e1122dff4722`,
+                    onSuccess: (data) => {
+                        widget.style.height = 666 + 'px';
+                        const weatherStatus = data.weather[0].main;
+                        widgetTemperature.textContent = `${Math.floor(data.main.temp - 273.15)} ºC`;
+                        widgetWeatherMain.textContent = weatherStatus[0].toUpperCase() + weatherStatus.slice(1);
+                        widgetWeatherStatus.src = weatherStatuses[weatherStatus.toLowerCase()];
+                        widgetWeatherStatus.classList.remove('widget__weather-status-hidden');
+                        widgetWind.textContent = `Ветер: ${data.wind.speed} м/c`;
+                    }
+                });
+            }
+        })
+    });
 });
 
 const noop = () => { };
