391458642d4d9a6f0d6735929b0327b72c415a50
flydecisive <flydecisive@yandex.ru>
2023-02-06 22:15:30 +0300

    Продолжение разработки игры

--- JS/skypro/rock_paper_scissor/js/script.js
+++ JS/skypro/rock_paper_scissor/js/script.js
@@ -1,5 +1,30 @@
 const backURL = 'https://skypro-rock-scissors-paper.herokuapp.com/';
 
+document.addEventListener('DOMContentLoaded', function () {
+    if (window.application.token === undefined) {
+        window.application.blocks['user-name'] = renderTextField;
+        window.application.blocks['login-button'] = renderLoginButton;
+        window.application.screens['login-screen'] = renderLoginScreen;
+        window.application.renderScreen('login-screen');
+    }
+
+    const button = document.querySelector('.login-button');
+    button.addEventListener('click', () => {
+        // request({
+        //     url: `${backURL}player-status`,
+        //     params: { token: `${window.application.token}` },
+        //     onSuccess: (data) => {
+        //         window.application.blocks['lobby-players'] = renderLobbyPlayers;
+        //         window.application.screens['lobby-screen'] = renderLobbyScreen;
+        //         window.application.renderScreen('lobby-screen');
+        //     }
+        // })
+        window.application.blocks['lobby-players'] = renderLobbyPlayers;
+        window.application.screens['lobby-screen'] = renderLobbyScreen;
+        window.application.renderScreen('lobby-screen');
+    })
+})
+
 function renderTextField(container) {
     const field = document.createElement('input');
     field.setAttribute('type', 'text');
@@ -12,12 +37,13 @@ function renderTextField(container) {
     container.appendChild(field);
 }
 
-function renderButton(container) {
+function renderLoginButton(container) {
     const button = document.createElement('button');
+    button.classList.add('login-button');
     button.setAttribute('type', 'submit');
     button.textContent = 'Войти';
 
-    button.addEventListener('click', () => {
+    button.addEventListener('submit', () => {
         const input = document.querySelector('.input');
         request({
             url: `${backURL}login`,
@@ -47,14 +73,47 @@ function renderLoginScreen() {
     app.appendChild(content);
 }
 
-window.application.blocks['user-name'] = renderTextField;
-window.application.blocks['login-button'] = renderButton;
-window.application.screens['login-screen'] = renderLoginScreen;
-window.application.renderScreen('login-screen');
+function renderLobbyScreen() {
+    const app = document.querySelector('.app');
+    app.textContent = '';
 
-// request({
-//     url: `${backURL}player-status?token=${window.application.token}`,
-//     onSuccess: (data) => {
-//         console.log(data);
-//     }
-// })
+    const title = document.createElement('h1');
+    title.textContent = 'Лобби';
+
+    const content = document.createElement('div');
+
+    window.application.renderBlock('lobby-players', content);
+
+    app.appendChild(title);
+    app.appendChild(content);
+}
+
+function renderLobbyPlayers(container) {
+    if (!(document.body.contains(document.querySelector('.player')))) {
+        for (let i = 1; i <= 3; i++) {
+            const player = document.createElement('p');
+            player.classList.add('player');
+            player.setAttribute('data-player', `${i}`);
+            container.appendChild(player);
+        }
+    }
+
+    request({
+        url: `${backURL}player-list`,
+        params: { token: window.application.token },
+        onSuccess: (data) => {
+            const players = document.querySelectorAll('.player');
+            players.forEach((player) => {
+                if (data.list[player.dataset.player] === undefined) {
+                    player.textContent = 'Нет игрока';
+                } else {
+                    // if (data.list[player.dataset.player].you === true) {
+                    //     console.log(data.list[player.dataset.player].login);
+                    // }
+                    player.textContent = data.list[player.dataset.player].login;
+                }
+            })
+        }
+    });
+}
+
