2b1df2f83ebe024fc0f088d67c769ed46da226d0
flydecisive <flydecisive@yandex.ru>
2023-02-08 22:36:01 +0300

    Игра
    Продолжаю разработку игры, дошел до экрана хода

--- JS/skypro/rock_paper_scissor/js/script.js
+++ JS/skypro/rock_paper_scissor/js/script.js
@@ -7,23 +7,116 @@ document.addEventListener('DOMContentLoaded', function () {
         window.application.screens['login-screen'] = renderLoginScreen;
         window.application.renderScreen('login-screen');
     }
-
-    const button = document.querySelector('.login-button');
-    button.addEventListener('click', () => {
-        // request({
-        //     url: `${backURL}player-status`,
-        //     params: { token: `${window.application.token}` },
-        //     onSuccess: (data) => {
-        //         window.application.blocks['lobby-players'] = renderLobbyPlayers;
-        //         window.application.screens['lobby-screen'] = renderLobbyScreen;
-        //         window.application.renderScreen('lobby-screen');
-        //     }
-        // })
-        window.application.blocks['lobby-players'] = renderLobbyPlayers;
-        window.application.screens['lobby-screen'] = renderLobbyScreen;
-        window.application.renderScreen('lobby-screen');
-    })
 })
+
+function renderStartButton(container) {
+    const button = document.createElement('button');
+    button.classList.add('start-button');
+    button.setAttribute('type', 'submit');
+    button.textContent = 'Старт';
+
+    button.addEventListener('click', function () {
+        request({
+            url: `${backURL}start`,
+            params: { token: `${window.application.token}` },
+            onSuccess: (data) => {
+                if (data['player-status'].status === 'game') {
+                    window.application.gameId = data['player-status'].game.id;
+                    window.application.blocks['waiting-game'] = renderWaitingBlock;
+                    window.application.screens['waiting-screen'] = renderWaitingScreen;
+                    window.application.renderScreen('waiting-screen');
+                }
+            }
+        });
+    });
+
+    container.appendChild(button);
+}
+
+function renderWaitingBlock(container) {
+    const text = document.createElement('p');
+    container.appendChild(text);
+    const waitingTimer = setInterval(function () {
+        request({
+            url: `${backURL}game-status`,
+            params: {
+                token: `${window.application.token}`,
+                id: `${window.application.gameId}`,
+            },
+            onSuccess: (data) => {
+                if (data['game-status'].status !== 'waiting-for-start') {
+                    window.application.blocks['rock'] = renderRockButton;
+                    window.application.blocks['paper'] = renderPaperButton;
+                    window.application.blocks['scissor'] = renderScissorButton;
+                    window.application.screens['game-screen'] = renderGameScreen;
+                    window.application.renderScreen('game-screen');
+                }
+            }
+        });
+    }, 500);
+    window.application.timers.push(waitingTimer);
+}
+
+function renderRockButton(container) {
+    const button = document.createElement('button');
+    button.textContent = 'камень';
+
+    button.addEventListener('click', function () {
+
+    });
+
+    container.appendChild(button);
+}
+
+function renderPaperButton(container) {
+    const button = document.createElement('button');
+    button.textContent = 'бумага';
+
+    button.addEventListener('click', function () {
+
+    });
+
+    container.appendChild(button);
+}
+
+function renderScissorButton(container) {
+    const button = document.createElement('button');
+    button.textContent = 'ножницы';
+
+    button.addEventListener('click', function () {
+
+    });
+
+    container.appendChild(button);
+}
+
+function renderGameScreen() {
+    const app = document.querySelector('.app');
+    app.textContent = '';
+
+    const title = document.createElement('h1');
+    title.textContent = 'Ход';
+
+    app.appendChild(title);
+    window.application.renderBlock('rock', app);
+    window.application.renderBlock('paper', app);
+    window.application.renderBlock('scissor', app);
+}
+
+function renderWaitingScreen() {
+    const app = document.querySelector('.app');
+    app.textContent = '';
+
+    const title = document.createElement('h1');
+    title.textContent = 'Ожидание игры';
+
+    const content = document.createElement('div');
+
+    window.application.renderBlock('waiting-game', content);
+
+    app.appendChild(title);
+    app.appendChild(content);
+}
 
 function renderTextField(container) {
     const field = document.createElement('input');
@@ -43,13 +136,31 @@ function renderLoginButton(container) {
     button.setAttribute('type', 'submit');
     button.textContent = 'Войти';
 
-    button.addEventListener('submit', () => {
+    button.addEventListener('click', () => {
         const input = document.querySelector('.input');
         request({
             url: `${backURL}login`,
             params: { login: input.value },
             onSuccess: (data) => {
                 window.application.token = data.token;
+                request({
+                    url: `${backURL}player-status`,
+                    params: { token: `${window.application.token}` },
+                    onSuccess: (data) => {
+                        if (data["player-status"].status === 'lobby') {
+                            window.application.blocks['lobby-players'] = renderLobbyPlayers;
+                            window.application.blocks['start-button'] = renderStartButton;
+                            window.application.screens['lobby-screen'] = renderLobbyScreen;
+                            window.application.renderScreen('lobby-screen');
+                        } else {
+                            window.application.blocks['rock'] = renderRockButton;
+                            window.application.blocks['paper'] = renderPaperButton;
+                            window.application.blocks['scissor'] = renderScissorButton;
+                            window.application.screens['game-screen'] = renderGameScreen;
+                            window.application.renderScreen('game-screen');
+                        }
+                    }
+                })
             }
         })
     });
@@ -81,39 +192,36 @@ function renderLobbyScreen() {
     title.textContent = 'Лобби';
 
     const content = document.createElement('div');
+    content.classList.add('players');
 
     window.application.renderBlock('lobby-players', content);
 
     app.appendChild(title);
     app.appendChild(content);
+    window.application.renderBlock('start-button', app);
 }
 
 function renderLobbyPlayers(container) {
-    if (!(document.body.contains(document.querySelector('.player')))) {
-        for (let i = 1; i <= 3; i++) {
-            const player = document.createElement('p');
-            player.classList.add('player');
-            player.setAttribute('data-player', `${i}`);
-            container.appendChild(player);
-        }
+    for (let i = 1; i <= 3; i++) {
+        const player = document.createElement('p');
+        player.classList.add('player');
+        container.appendChild(player);
     }
-
-    request({
-        url: `${backURL}player-list`,
-        params: { token: window.application.token },
-        onSuccess: (data) => {
-            const players = document.querySelectorAll('.player');
-            players.forEach((player) => {
-                if (data.list[player.dataset.player] === undefined) {
-                    player.textContent = 'Нет игрока';
-                } else {
-                    // if (data.list[player.dataset.player].you === true) {
-                    //     console.log(data.list[player.dataset.player].login);
-                    // }
-                    player.textContent = data.list[player.dataset.player].login;
+    const lobbyTimer = setInterval(function () {
+        request({
+            url: `${backURL}player-list`,
+            params: { token: `${window.application.token}` },
+            onSuccess: (data) => {
+                const players = document.querySelectorAll('.player');
+                for (let i = 0; i < 3; i++) {
+                    if (data.list[data.list.length - (i + 1)] === undefined) {
+                        players[i].textContent = 'Нет игрока';
+                    } else {
+                        players[i].textContent = data.list[data.list.length - (i + 1)].login;
+                    }
                 }
-            })
-        }
-    });
+            }
+        });
+    }, 1000)
+    window.application.timers.push(lobbyTimer);
 }
-
