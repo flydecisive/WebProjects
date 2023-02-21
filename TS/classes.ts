591afe4634f7025e28b7034018c30e7d32365e40
flydecisive <flydecisive@yandex.ru>
2023-02-21 19:40:14 +0300

    Осваиваю классы в TS

--- TS/classes.ts
+++ TS/classes.ts
@@ -1,23 +1,46 @@
 class Vehicle {
-    drive(): void {
-        console.log(`I'm driving`);
+    // public drive(): void {
+    //     console.log(`I'm driving`);
+    // }
+
+    // color: string; 
+
+    // constructor(color: string) {
+    //     this.color = color;
+    // }
+
+    constructor(public color: string) {}
+
+    public stop(): void {
+        console.log(`I've stopped`);
     }
 
-    stop(): void {
-        console.log(`I've stopped`);
+    protected beep(): void {
+        console.log(`Beeeeeeeep!`);
     }
 };
 
 class Car extends Vehicle {
-    drive(): void {
+
+    constructor(public wheels: number, color: string) {
+        super(color);
+    }
+
+    private drive(): void {
         console.log(`I'm a car and I'm driving`);
+    }
+
+    startDrivingProcess(): void {
+        this.drive();
+        this.beep();
     }
 }
 
-const vehicle = new Vehicle();
-vehicle.drive();
+const vehicle = new Vehicle('black');
+// vehicle.drive();
+console.log(vehicle.color);
 vehicle.stop();
 
-const car = new Car();
-car.drive();
+const car = new Car(3, 'red');
+car.startDrivingProcess();
 car.stop();
